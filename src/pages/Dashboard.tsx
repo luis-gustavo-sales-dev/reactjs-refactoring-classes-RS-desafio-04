a43622ffdeda24d940c6ab8cf9bd50188c32e60f
Luis Gustavo <luisgssf@gmail.com>
2021-12-13 19:23:30 -0300

    Realizacoes: rotas, estilo global, api axios, pacotes instalados

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const Dashboard = (): JSX.Element => {
+    //[foods, setFoods] = useState()
+    return (<>
+        <h1>Dashboard</h1>
+    </>)
+}
+
+export default Dashboard;
