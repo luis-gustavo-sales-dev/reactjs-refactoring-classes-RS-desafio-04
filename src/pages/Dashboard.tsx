35a238364bf0a8f86640ec321cbfaca1f381eadb
Luis Gustavo <luisgssf@gmail.com>
2021-12-14 11:17:26 -0300

    Adicionar comida em Dashboard funcionando

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import api from "../services/api";
 
 interface Food {
-    id: number,
+    id?: number,
     name: string,
     description: string,
     price: string,
-    available: boolean,
+    available?: boolean,
     image: string
 }
 const Dashboard = (): JSX.Element => {
@@ -17,18 +17,42 @@ const Dashboard = (): JSX.Element => {
             console.log("loadFoods")
             let f = await api.get(`/foods`)
                         .then( (response) => response.data)
-            console.log(f)
+            //console.log(f)
             setFoods([...f])
-            console.log(foods)
+            //console.log(foods)
         }
         loadFoods()
     }, [])
 
+    async function handleAddFood(food: Food) {
+        try {
+            const response = await api.post(`/foods`, {
+                ...food,
+                available: true
+            })
+            // console.log(response.data)
+            setFoods([response.data, ...foods])
+        } catch {
+            // Use o toast aqui para ficar legal
+        }
+    }
+
+    async function testeAddFood() {
+        let tempFood = {
+            "name": "aaaa",
+            "description": "aaaaaaaa",
+            "price": "123",
+            "image": "hhttp"
+        }
+        await handleAddFood(tempFood)
+    }
+
     return (<>
         <h1>Dashboard</h1>
         { foods.map( (f) => {
-            return <li>{f.name}</li>
+            return <li key={f.id}>{f.name}</li>
         })}
+        <button onClick={testeAddFood}>Criar comida</button>
     </>)
 }
 
