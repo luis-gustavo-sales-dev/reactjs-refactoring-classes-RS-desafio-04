02c6af501969b5c41fd95ce99a6cc7f9954887a2
Luis Gustavo <luisgssf@gmail.com>
2021-12-14 11:37:39 -0300

    Funçao de atualizar comida no Dashboard funciona e atualizar o state de foods

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,6 +11,7 @@ interface Food {
 }
 const Dashboard = (): JSX.Element => {
     const [foods, setFoods] = useState<Food[]>([])
+    const [editingFood, setEditingFood] = useState<Food>()
 
     useEffect( () => {
         async function loadFoods () {
@@ -37,6 +38,32 @@ const Dashboard = (): JSX.Element => {
         }
     }
 
+    async function handleUpdateFood(food: Food) {
+        try {
+            const foodUpdated: Food = await api.put(`/foods/${5}`, {
+                "name": "bbbbbb",
+                "description": "bbbbbbb",
+                "price": "123",
+                "image": "hhttp"
+            }).then( response => response.data)
+
+            const foodsUpdated = foods.map( f => 
+                f.id === foodUpdated.id ? foodUpdated : f
+            )
+
+            setFoods([...foodsUpdated])
+
+            /*
+            let f = await api.get(`/foods`)
+                        .then( (response) => response.data)
+            setFoods([...f])
+            */
+            
+        } catch {
+            // Use o toast aqui para ficar legal
+        }
+    }
+
     async function testeAddFood() {
         let tempFood = {
             "name": "aaaa",
@@ -44,13 +71,13 @@ const Dashboard = (): JSX.Element => {
             "price": "123",
             "image": "hhttp"
         }
-        await handleAddFood(tempFood)
+        await handleUpdateFood(tempFood)
     }
 
     return (<>
         <h1>Dashboard</h1>
         { foods.map( (f) => {
-            return <li key={f.id}>{f.name}</li>
+            return <li key={f.id}>{f.id + " " +f.name}</li>
         })}
         <button onClick={testeAddFood}>Criar comida</button>
     </>)
